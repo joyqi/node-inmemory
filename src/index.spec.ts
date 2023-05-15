c8df591a7186e2df63ac0a31f587eed4f5eeafd0
joyqi <joyqi@segmentfault.com>
2023-05-15 17:34:35 +0800

    add wait value

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -18,4 +18,44 @@ describe('inMemory', () => {
         reset();
         assert.strictEqual(await get(), 1);
     });
+
+    it('should wait for a promise to resolve', async () => {
+        let counter = 0;
+        const [get] = inMemory(() => new Promise((resolve) => {
+            setTimeout(() => {
+                counter += 1;
+                resolve(1);
+            }, 100)
+        }));
+
+        assert.deepStrictEqual(await Promise.all([get(), get(), get()]), [1, 1, 1]);
+        assert.strictEqual(counter, 1);
+    });
+
+    it('should throw an error if the promise rejects', async () => {
+        let counter = 0;
+        const [get] = inMemory(() => new Promise((_, reject) => {
+            setTimeout(() => {
+                counter += 1;
+                reject(new Error('test'));
+            }, 100)
+        }));
+
+        get().catch(e => {
+            assert.strictEqual(e.message, 'test');
+        });
+
+        try {
+            await get();
+            assert.fail('should have thrown');
+        } catch (e) {
+            if (e instanceof Error) {
+                assert.strictEqual(e.message, 'test');
+            } else {
+                assert.fail('should have thrown an error');
+            }
+        }
+
+        assert.strictEqual(counter, 1);
+    });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 type StoreItem = {
     value: any;
     expires: number;
+    refreshed: boolean;
 };
 
 // Store the values in a WeakMap.
@@ -11,37 +12,76 @@ function setValue(key: object, value: any, ttl = 0) {
     storage.set(key, {
         value,
         expires: ttl > 0 ? Date.now() + ttl : 0,
+        refreshed: false,
     });
+}
+
+// Wait for a value to be set.
+function wait<T>() {
+    let waitResolve: (value: any) => void;
+    let waitReject: (e: any) => void;
+
+    const value = new Promise<T>((resolve, reject) => {
+        waitResolve = resolve;
+        waitReject = reject;
+    });
+
+    const cbResolve = (value: any) => {
+        if (waitResolve) {
+            waitResolve(value);
+        }
+    };
+
+    const cbReject = (e: any) => {
+        if (waitReject) {
+            waitReject(e);
+        }
+    };
+
+    return [value, cbResolve, cbReject] as const;
 }
 
 // Generate a getter and a resetter for a function.
 // The getter will cache the result of the function.
 // The resetter will clear the cache.
 export function inMemory<T extends any, ReturnValue extends T | Promise<T>>(fn: () => ReturnValue, ttl = 0): [() => ReturnValue, (value?: any) => void] {
-    const getter = () => {
-        const item = storage.get(fn);
-        if (item && (item.expires === 0 || item.expires > Date.now())) {
-            return item.value;
-        }
-
-        const returnValue = fn();
-
-        if (returnValue instanceof Promise) {
-            return returnValue.then((value) => {
-                setValue(fn, value, ttl);
-                return value;
-            });
-        } else {
-            setValue(fn, returnValue, ttl);
-            return returnValue;
-        }
-    };
-
     const setter = (value?: any) => {
         if (typeof value !== 'undefined') {
             setValue(fn, value, ttl);
         } else {
             storage.delete(fn);
+        }
+    };
+
+    const getter = () => {
+        const item = storage.get(fn);
+        if (item) {
+            if (item.expires === 0 || item.expires > Date.now() || item.refreshed) {
+                return item.value;
+            }
+
+            // allow only one refresh per ttl
+            item.refreshed = true;
+        }
+
+        const returnValue = fn();
+
+        if (returnValue instanceof Promise) {
+            const [v, cbResolve, cbReject] = wait<T>();
+            setValue(fn, v);
+
+            return returnValue.then((value) => {
+                cbResolve(value);
+                setter(value);
+                return value;
+            }).catch(e => {
+                cbReject(e);
+                setter();
+                throw e;
+            });
+        } else {
+            setter(returnValue);
+            return returnValue;
         }
     };
 
